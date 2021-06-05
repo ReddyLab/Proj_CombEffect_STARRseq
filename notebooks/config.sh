cf27baf9a46686b9de06f6dd8c03aaad55223cde
Kuei Yueh Ko <kk319@dcc-chsi-22.rc.duke.edu>
2021-06-05 11:23:24 -0400

    update 2021/06/05

--- notebooks/config.sh
+++ notebooks/config.sh
@@ -1,5 +1,13 @@
-FD_WRK=/data/reddylab/Kuei
-FD_SRC=${FD_WRK}/source
-FD_OUT=${FD_WRK}/out/CombEffect_STARR
+### FD_WRK=/data/reddylab/Kuei
+if echo $(pwd) | grep -q "gpfs"; then
+    FD_WRK="/data/reddylab/Kuei/out/CombEffect_STARR"
+    FD_BASE="/data/reddylab/Kuei"
+fi
+if echo $(pwd) | grep -q "hpc"; then
+    FD_WRK="/work/kk319/out/CombEffect_STARR"
+    FD_BASE="/work/kk319"
+fi
 
-FD_DAT=/data/reddylab/gjohnson/whole_genome_STARRseq/wgss3/alignment_and_processing/alignments
+#FD_SRC=${FD_WRK}/source
+#FD_OUT=${FD_WRK}/out/CombEffect_STARR
+#FD_DAT=/data/reddylab/gjohnson/whole_genome_STARRseq/wgss3/alignment_and_processing/alignments
